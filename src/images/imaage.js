4f43738e09c79f0355dda23042d6928a369ef27e
gabinacodes <badruraidah@gmail.com>
2023-07-25 15:21:55 +0100

    update

---
+++ src/images/imaage.js
@@ -0,0 +1,23 @@
+import ali from './avatar-ali.png'
+import anisha from './avatar-anisha.png'
+import richard from './avatar-richard.png'
+import shanai from './avatar-shanai.png'
+import bgDesktop from './bg-simplify-section-desktop.svg'
+import bgMobile from './bg-simplify-section-mobile.svg'
+import bgTab from './bg-tablet-pattern.svg'
+import close from './icon-close.svg'
+import facebook from './icon-facebook.svg'
+import hamburger from './icon-hamburger.svg'
+import instagram from './icon-instagram.svg'
+import pinterest from './icon-pinterest.svg'
+import twitter from './icon-twitter.svg'
+import youtube from './icon-youtube.svg'
+import illustration from './illustration-intro.svg'
+import logoWhite from './logo-white.png'
+import logo from './logo.png'
+
+const image =[
+    ali,anisha,richard,shanai,bgDesktop,bgMobile,bgTab,close,facebook,hamburger,instagram,pinterest,twitter,youtube,illustration,logoWhite,logo
+]
+
+export default image
---
+++ src/page/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <div>
+      <div class="text-xl m-3 font-medium text-blue">ChitChat</div>
+      </div>
+    </div>
+  )
+}
+
+export default Home 
